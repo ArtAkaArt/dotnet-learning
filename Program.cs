0774936c39c7f59b8a655c3890c5356936bc6acb
ArtAkaArt <91897648+ArtAkaArt@users.noreply.github.com>
2022-01-09 19:43:55 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -8,35 +8,41 @@ using System.Linq;
 namespace Solution
 {
     class Program
-    {
+    {    
         static void Main(string[] args)
         {
-            string buffer = "";
-            using (StreamReader sr = new StreamReader($"{Directory.GetCurrentDirectory()}\\JSON_sample_1.json"))
-            {
-                    buffer += sr.ReadToEnd();
-            }
-            List<Deal> DealArray = JsonConvert.DeserializeObject<List<Deal>>(buffer);
-            IList<string> selectedDeals =  GetNumbersOfDeals(DealArray);
+            var buffer = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\JSON_sample_1.json");
+            var DealArray = JsonConvert.DeserializeObject<List<Deal>>(buffer);
+            var selectedDeals = GetNumbersOfDeals(DealArray);
             Console.Write($"Количество значений: {selectedDeals.Count}");
             foreach (string item in selectedDeals)
             {
                 Console.Write($", {item}");
             }
-
+            Console.WriteLine();
+            var SumByMonth = GetSumsByMonth(DealArray);
+            foreach (var t in SumByMonth){
+                Console.WriteLine($"Номер месяца: {t.Month.Month}, общая сумма сделок: {t.Sum}");
+            }
         }
-      static IList<string> GetNumbersOfDeals(IEnumerable<Deal> deals) // изменил сигнатуру с IList<int> на IList<string> т.к. Id представлены как string
+        static IList<string> GetNumbersOfDeals(IEnumerable<Deal> deals)
         {
-            var selectedDeals = from t in deals
-                                orderby t.Date
-                                where t.Sum > 100
-                                select t;
+            
+            IList<string> result = deals.Where(t=> t.Sum>100)
+                                    .OrderBy(t => t.Date).Take(5)
+                                    .OrderByDescending(t=>t.Sum)
+                                    .Select(t => t.Id)
+                                    .ToList();
+            return result;
+        }
+        static IList<SumByMonth> GetSumsByMonth(IEnumerable<Deal> deals)
+        {
+            IList<SumByMonth> result = new List<SumByMonth>();
 
-            var sortedDeals = selectedDeals.OrderByDescending(u=>u.Sum).Take(3);
-            IList<string> result = new List<string>();
-            foreach (Deal deal in sortedDeals){
-                result.Add(deal.Id);
-            }  
+            result = deals.GroupBy(t => t.Date.Month)
+                        .Select(cl => new SumByMonth{Month = cl.First().Date, Sum = cl.Sum(c => c.Sum)})
+                        .ToList();
+
             return result;
         }
     }
@@ -46,6 +52,9 @@ namespace Solution
         public string Id {get;set;}
         public DateTime Date {get;set;}
     }
-
-
+    class SumByMonth{ 
+        public int Sum {get;set;}
+        public DateTime Month {get;set;}
+    }
+    //record SumByMonth(DateTime Month, int Sum); не получалось инициализировать рекорд в ст. 43, переделал в класс. Не было конструктора без параметров?
 }
