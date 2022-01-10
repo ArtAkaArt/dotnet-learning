5f4f1cf73443b2bca0195c1e621079d10165250a
Art <bartalamu@list.ru>
2022-01-10 20:10:27 +0500

    test

--- Program.cs
+++ Program.cs
@@ -12,49 +12,36 @@ namespace Solution
         static void Main(string[] args)
         {
             var buffer = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\JSON_sample_1.json");
-            var DealArray = JsonConvert.DeserializeObject<List<Deal>>(buffer);
-            var selectedDeals = GetNumbersOfDeals(DealArray);
+            var dealsArray = JsonConvert.DeserializeObject<List<Deal>>(buffer);
+            var selectedDeals = GetNumbersOfDeals(dealsArray);
             Console.Write($"Количество значений: {selectedDeals.Count}");
-            foreach (string item in selectedDeals)
+            foreach (var item in selectedDeals)
             {
                 Console.Write($", {item}");
             }
             Console.WriteLine();
-            var SumByMonth = GetSumsByMonth(DealArray);
-            foreach (var t in SumByMonth){
+            var sumByMonth = GetSumsByMonth(dealsArray);
+            foreach (var t in sumByMonth)
+            {
                 Console.WriteLine($"Номер месяца: {t.Month.Month}, общая сумма сделок: {t.Sum}");
             }
-        }
-        static IList<string> GetNumbersOfDeals(IEnumerable<Deal> deals)
-        {
+
+            IList<string> GetNumbersOfDeals(IEnumerable<Deal> deals)
+            { 
+                return deals.Where(t=> t.Sum>100)
+                            .OrderBy(t => t.Date)
+                            .Take(5)
+                            .OrderByDescending(t=>t.Sum)
+                            .Select(t => t.Id)
+                            .ToList();
+            }
             
-            IList<string> result = deals.Where(t=> t.Sum>100)
-                                    .OrderBy(t => t.Date).Take(5)
-                                    .OrderByDescending(t=>t.Sum)
-                                    .Select(t => t.Id)
-                                    .ToList();
-            return result;
-        }
-        static IList<SumByMonth> GetSumsByMonth(IEnumerable<Deal> deals)
-        {
-            IList<SumByMonth> result = new List<SumByMonth>();
-
-            result = deals.GroupBy(t => t.Date.Month)
-                        .Select(cl => new SumByMonth{Month = cl.First().Date, Sum = cl.Sum(c => c.Sum)})
-                        .ToList();
-
-            return result;
+            IList<SumByMonth> GetSumsByMonth(IEnumerable<Deal> deals)
+            {
+                return deals.GroupBy(t => t.Date.Month)
+                            .Select(cl => new SumByMonth{Month = cl.First().Date, Sum = cl.Sum(c => c.Sum)})
+                            .ToList();
+            }
         }
     }
-    class Deal
-    {
-        public int Sum {get;set;}
-        public string Id {get;set;}
-        public DateTime Date {get;set;}
-    }
-    class SumByMonth{ 
-        public int Sum {get;set;}
-        public DateTime Month {get;set;}
-    }
-    //record SumByMonth(DateTime Month, int Sum); не получалось инициализировать рекорд в ст. 45, переделал в класс. Не было конструктора без параметров?
 }
---
+++ SumByMonth.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Solution
+{
+    public class SumByMonth
+    {
+        public int Sum { get; set; }
+        public DateTime Month { get; set; }
+    }
+}
