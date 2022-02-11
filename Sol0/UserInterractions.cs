a54db22b04269f8f9767d4dcca2c35785d7687e1
Art <bartalamu@list.ru>
2022-02-12 01:49:41 +0500

    v1.4 CRUD набросок

--- Sol0/UserInterractions.cs
+++ Sol0/UserInterractions.cs
@@ -1,10 +1,13 @@
-﻿namespace Sol0
+﻿using System.Text.Json;
+using System.Text;
+
+namespace Sol0
 {
     public class UserInterractions
     {
         public event EventHandler<CustomEventArgs> Notify; //нашел как предустановленный делегат использовать с кастомным arg
 
-        internal void SerachUnits(IEnumerable<Unit> units, IEnumerable<Tank> tanks, IEnumerable<Factory> factories)
+        internal void SerachUnits(List<Unit> units, List<Tank> tanks, List<Factory> factories)
         {
             Console.WriteLine("Введите название резервуара");
             var unitName = Console.ReadLine();
@@ -16,7 +19,27 @@
                     var foundUnit = FindUnit(units, tanks, unitName);
                     var factory = FindFactory(factories, foundUnit);
 
-                    Console.WriteLine($"{unitName} принадлежит установке {foundUnit.Name} и заводу {factory.Name}");
+                    Console.WriteLine($"\n{unitName} принадлежит установке {foundUnit.Name} и заводу {factory.Name}\n"); //тоже своего рода Read
+                    Console.WriteLine("Для информации о всех установках, Заводах и резервуаров введите R" +
+                                    "\nДля создания новой установки введите С" +
+                                    "\nДля изменения сведений об установке введите U" +
+                                    "\nДля удаления установки введите D");
+                    var responce = Console.ReadLine();
+                    switch (responce)
+                    {
+                        case "C":
+                            CreateUnit(units);
+                            break;
+                        case "R":
+                            ReadFacility(units, tanks, factories);
+                            break;
+                        case "U":
+                            UpdateUnit(units);
+                            break;
+                        case "D":
+                            DeleteUnit(units);
+                            break;
+                    }
                     Console.WriteLine("Повторите поиск или введите - для завершения");
                     unitName = Console.ReadLine();
                 }
@@ -65,6 +88,80 @@
             if (factory is null)
                 throw new InvalidOperationException($"Не найден завод у установки с именем {unit.Name}");
             return factory;
+        }
+        //
+        void CreateUnit(List<Unit> list)
+        {
+            Console.WriteLine("C_ВВедите через запятую значения для Id, Name и FactoryId");
+            var unitStats = Console.ReadLine();
+            var stats = unitStats.Split(',');
+
+            if (stats.Length == 3 && Int32.TryParse(stats[0], out int stat1) && Int32.TryParse(stats[2], out int stat3))
+            {
+                list.Add(new Unit { Id = stat1, Name = stats[1], FactoryId = stat3 });
+            }
+            SaveToFile(list);
+        }
+        void DeleteUnit(List<Unit> list) 
+        {
+            list.ForEach(t => Console.WriteLine($"Id = {t.Id}, Name = {t.Name}, FactoryId = {t.FactoryId}"));
+            Console.WriteLine("D_Выберите Id установки для удаления");
+            int.TryParse(Console.ReadLine(), out int id);
+            var unit = list.FirstOrDefault(t =>t.Id == id);
+            if (unit is not null)
+                list.Remove(unit);
+            SaveToFile(list);
+        }
+        void UpdateUnit(List<Unit> list)
+        {
+            list.ForEach(t => Console.WriteLine($"Id = {t.Id}, Name = {t.Name}, FactoryId = {t.FactoryId}"));
+            Console.WriteLine("U_Выберите Id установки для изменения");
+            int.TryParse(Console.ReadLine(), out int id);
+            var unit = list.FirstOrDefault(t => t.Id == id);
+            Console.WriteLine("C_ВВедите через запятую значения для Id, Name и FactoryId");
+            var unitStats = Console.ReadLine();
+            var stats = unitStats.Split(',');
+            if (unit is not null && Int32.TryParse(stats[0], out int stat1) && Int32.TryParse(stats[2], out int stat3))
+            {
+                unit.Id = stat1;
+                unit.Name = stats[1];
+                unit.FactoryId = stat3;
+            }
+            SaveToFile(list);
+        }
+        void ReadFacility(List<Unit> units, List<Tank> tanks, List<Factory> factories)
+        {
+            Console.WriteLine("R_Введите Unit, Tank или Facility для информации о хранящихся данных");
+            var responce = Console.ReadLine();
+            switch (responce)
+            {
+                case "Unit":
+                    units.ForEach(t => Console.WriteLine($"Id = {t.Id}, Name = {t.Name}, FactoryId = {t.FactoryId}"));
+                    break;
+                case "Tank":
+                        tanks.ForEach(t => Console.WriteLine($"Id = {t.Id}, Name = {t.Name}, UnitId = {t.UnitId}, Volume = {t.Volume}, MaxVolume = {t.MaxVolume}"));
+                        break;
+                case "Facility":
+                    factories.ForEach(t => Console.WriteLine($"Id = {t.Id}, Name = {t.Name}, Description = {t.Description}"));
+                    break;
+            }
+        }
+
+        //блок методов сохранения файлов после CRUD операций
+        internal void SaveToFile(List<Unit> list)
+        {
+            var content = JsonSerializer.Serialize(list);
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(),"..\\..\\..\\Jsons\\UnitsTst.json"), content, Encoding.UTF8);
+        }
+        internal void SaveToFile(List<Factory> list)
+        {
+            var content = JsonSerializer.Serialize(list);
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\Jsons\\FactoriesTst.json"), content, Encoding.UTF8);
+        }
+        internal void SaveToFile(List<Tank> list)
+        {
+            var content = JsonSerializer.Serialize(list);
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\Jsons\\TanksTst.json"), content, Encoding.UTF8);
         }
     }
     public class CustomEventArgs : EventArgs
