5cfd9eccc5892e52fad8bd21eb2ec4895090a397
Art <bartalamu@list.ru>
2022-02-22 00:00:22 +0500

    v1 задача на алгоритмы, поиск сдивга

---
+++ Sol2/Sol2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Solution
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var part1 = Enumerable.Range(15_000, 100_000); // 15000, 15001, ... 114998, 114999
+            var part2 = Enumerable.Range(0, 14_995); // 0, 1, ... 14993, 14994
+            var sample = new[] { 15, 16, 18, 20, 1, 2, 5, 6, 7, 8, 11, 12 };
+
+            Console.WriteLine($"В массиве сдвиг равен = {Array.IndexOf(sample, FindLowest(sample))}");
+
+        }
+
+        static int FindLowest(int[] arr)
+        {
+            if (arr[0] < arr[arr.Length - 1]) return arr[0];
+            var result = int.MaxValue;
+            if (arr.Length == 3 || arr.Length == 2) 
+            {
+                foreach (int t in arr)
+                {
+                    if (t < result)
+                    { 
+                        result = t;
+                    }
+                }
+            }
+            var end = arr.Length / 2;
+            var start = arr.Length / 2 + 1;
+
+            if (arr[0] > arr[end-1])
+            {
+                var firstHalf = new int[end];
+                Array.Copy(arr, firstHalf, end);
+                result = FindLowest(firstHalf);
+            }
+            else if (arr[start-1] > arr[arr.Length - 1])
+            {
+                var secondHalf = new int[arr.Length - end];
+                Array.Copy(arr, end, secondHalf, 0, arr.Length - end);
+                result = FindLowest(secondHalf);
+            }
+            else if (arr[end-1] > arr[start-1]) result = arr[end];
+            return result;
+        }
+    }
+}
