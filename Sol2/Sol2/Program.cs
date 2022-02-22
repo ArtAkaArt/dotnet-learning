ddb1dae50d999efc9421ef2eb8de8b4dc487ee29
Art <bartalamu@list.ru>
2022-02-22 21:34:56 +0500

    v1.1.1 не рекурсивный -багуля

--- Sol2/Sol2/Program.cs
+++ Sol2/Sol2/Program.cs
@@ -8,14 +8,21 @@
             var part2 = Enumerable.Range(0, 14_995);
             var sample = part1.Concat(part2).ToArray(); //expected: 100_000
             var sample1 = new[] { 15, 16, 18, 20, 1, 2, 5, 6, 7, 8, 11, 12 }; // expected: 4
-            var sample2 = new[] { 5, 6, 7,     8, 11, 12,     15, 16, 18,     20, 1, 2 }; // expected: 10
+            var sample2 = new[] { 5, 6, 7,     8, 11, 12,     15, 16, 18,     1, 2, 3 }; // expected: 9
             var sample3 = new[] { 1, 2, 5, 6, 7, 8, 11, 12, 15, 16, 18, 20 }; // expected: 0
             var sample4 = new[] { 11, 1, 2, 5 }; //малый четный массив, expected: 1
             var sample5 = new[] { 7, 11, 1, 2, 5 }; //малый нечетный массив, expected: 2
             var sample6 = new[] { 5, 6, 1, 2}; // малый со сдвигом в центре, expected: 2
             var sample7 = new[] { 5 }; // expected: 0
 
-            Console.WriteLine($"В массиве сдвиг равен = {GetShiftPositionAlt(sample)}");
+            Console.WriteLine($"В массиве sample сдвиг равен = {GetShiftPositionAlt(sample)}");
+            Console.WriteLine($"В массиве sample1 сдвиг равен = {GetShiftPositionAlt(sample1)}");
+            Console.WriteLine($"В массиве sample2 сдвиг равен = {GetShiftPositionAlt(sample2)}");
+            Console.WriteLine($"В массиве sample3 сдвиг равен = {GetShiftPositionAlt(sample3)}");
+            Console.WriteLine($"В массиве sample4 сдвиг равен = {GetShiftPositionAlt(sample4)}");
+            Console.WriteLine($"В массиве sample5 сдвиг равен = {GetShiftPositionAlt(sample5)}");
+            Console.WriteLine($"В массиве sample6 сдвиг равен = {GetShiftPositionAlt(sample6)}");
+            Console.WriteLine($"В массиве sample7 сдвиг равен = {GetShiftPositionAlt(sample7)}");
         }
         static int GetShiftPositionAlt(int[] arr)
         {
@@ -26,7 +33,7 @@
             {
                 if (arr[start] > arr[((end-start) / 2)+start]) end = start + (end-start)/2; 
                 else if (arr[((end - start) / 2) + start + 1] > arr[end]) start = ((end - start) / 2) + start + 1;
-                else if (arr[((end - start) / 2) + start ] > arr[((end - start) / 2) + start + 1]) return end / 2;
+                else if (arr[((end - start) / 2) + start ] > arr[((end - start) / 2) + start + 1]) return ((end - start) / 2) + start + 1; //багуля
             }
             var buffer = int.MaxValue;
             int result = 0;
