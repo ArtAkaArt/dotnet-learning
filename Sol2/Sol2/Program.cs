71af056fadc5b194f1ca0397eb5158462689d927
Art <bartalamu@list.ru>
2022-02-22 21:21:25 +0500

    @ArtAkaArt v1.1 не рекурсивный метод (М - МАТЕМАТИКА)

--- Sol2/Sol2/Program.cs
+++ Sol2/Sol2/Program.cs
@@ -8,16 +8,38 @@
             var part2 = Enumerable.Range(0, 14_995);
             var sample = part1.Concat(part2).ToArray(); //expected: 100_000
             var sample1 = new[] { 15, 16, 18, 20, 1, 2, 5, 6, 7, 8, 11, 12 }; // expected: 4
-            var sample2 = new[] { 5, 6, 7, 8, 11, 12, 15, 16, 18, 20, 1, 2 }; // expected: 10
+            var sample2 = new[] { 5, 6, 7,     8, 11, 12,     15, 16, 18,     20, 1, 2 }; // expected: 10
             var sample3 = new[] { 1, 2, 5, 6, 7, 8, 11, 12, 15, 16, 18, 20 }; // expected: 0
             var sample4 = new[] { 11, 1, 2, 5 }; //малый четный массив, expected: 1
             var sample5 = new[] { 7, 11, 1, 2, 5 }; //малый нечетный массив, expected: 2
             var sample6 = new[] { 5, 6, 1, 2}; // малый со сдвигом в центре, expected: 2
             var sample7 = new[] { 5 }; // expected: 0
 
-            Console.WriteLine($"В массиве сдвиг равен = {GetShiftPosition(sample7)}");
+            Console.WriteLine($"В массиве сдвиг равен = {GetShiftPositionAlt(sample)}");
         }
-
+        static int GetShiftPositionAlt(int[] arr)
+        {
+            if (arr[0] < arr[arr.Length - 1] || arr.Length == 1) return 0;
+            var start = 0;
+            var end = arr.Length-1;
+            while (end - start > 3)
+            {
+                if (arr[start] > arr[((end-start) / 2)+start]) end = start + (end-start)/2; 
+                else if (arr[((end - start) / 2) + start + 1] > arr[end]) start = ((end - start) / 2) + start + 1;
+                else if (arr[((end - start) / 2) + start ] > arr[((end - start) / 2) + start + 1]) return end / 2;
+            }
+            var buffer = int.MaxValue;
+            int result = 0;
+            for (int i = start; i<=end; i++)
+            {
+                if (arr[i] < buffer)
+                {
+                    buffer = arr[i];
+                    result = i;
+                }
+            }
+            return result;
+        }
         static int FindLowest(int[] arr)
         {
             if (arr[0] < arr[arr.Length - 1] || arr.Length == 1) return arr[0];
@@ -55,5 +77,6 @@
         {
             return Array.IndexOf(arr, FindLowest(arr));
         }
+        
     }
 }
