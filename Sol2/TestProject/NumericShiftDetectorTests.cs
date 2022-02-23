0dc76bf249f61753d623e478b94fe06f21ce64a9
Art <bartalamu@list.ru>
2022-02-23 23:30:19 +0500

    v1.1.5 тесты через MemeberData

--- Sol2/TestProject/NumericShiftDetectorTests.cs
+++ Sol2/TestProject/NumericShiftDetectorTests.cs
@@ -1,74 +1,33 @@
 ﻿using Xunit;
 using System.Linq;
 using Solution;
-
+using System.Collections.Generic;
 
 namespace TestProject
 {
     public class NumericShiftDetectorTests
     {
-		[Fact]
-		public void TestSample1()
+		[Theory]
+		[MemberData(nameof(ArraysAndShifts))]
+		public void Should_detect_shifts(int[] sample, int result) 
 		{
-			
-			var sample = new[] { 15, 16, 18, 20, 1, 2, 5, 6, 7, 8, 11, 12 };
-			var result = Program.GetShiftPositionAlt(sample);
-			Assert.Equal(expected: 4, actual: result);
-		}
-		[Fact]
-		public void TestSample2()
-		{
-			var sample = new[] { 5, 6, 7, 8, 11, 12, 15, 16, 18, 1, 2, 3 };
-			var result = Program.GetShiftPositionAlt(sample);
-			Assert.Equal(expected: 9, actual: result);
-		}
-		[Fact]
-		public void TestSample3()
-		{
-			var sample = new[] { 5, 6, 7, 8, 11, 12, 15, 16, 18, 1, 2, 3 };
-			var result = Program.GetShiftPositionAlt(sample);
-			Assert.Equal(expected: 9, actual: result);
-		}
-		[Fact]
-		public void TestSample4()
-		{
-			var sample = new[] { 5, 6, 7, 8, 11, 12, 15, 16, 18, 20, 1, 2 };
-			var result = Program.GetShiftPositionAlt(sample);
-			Assert.Equal(expected: 10, actual: result);
-		}
-		[Fact]
-		public void TestSample5()
-		{
-			var sample = new[] { 5, 6, 1, 2 };
-			var result = Program.GetShiftPositionAlt(sample);
-			Assert.Equal(expected: 2, actual: result);
-		}
-		[Fact]
-		public void TestSample6()
-		{
-			var sample = new[] { 7, 11, 1, 2, 5 };
-			var result = Program.GetShiftPositionAlt(sample);
-			Assert.Equal(expected: 2, actual: result);
+			Assert.Equal(expected: result, actual: Program.GetShiftPositionAlt(sample));
 		}
 
-		[Fact]
-		public void TestSampleWithoutShift()
+		public static IEnumerable<object[]> ArraysAndShifts()
 		{
-			var sample = new[] { 1, 2, 5, 6, 7, 8, 11, 12, 15, 16, 18, 20 };
-			var result = Program.GetShiftPositionAlt(sample);
-			Assert.Equal(expected: 0, actual: result);
-		}
-
-		[Fact]
-		public void TestLargeSample()
-		{
-			var part1 = Enumerable.Range(15_000, 100_000); // 15000, 15001, ... 114998, 114999
-			var part2 = Enumerable.Range(0, 14_995); // 0, 1, ... 14993, 14994
-			var sample = part1.Concat(part2).ToArray(); // объединение двух последовательностей // 15000, 15001, ... 114998, 114999, 0, 1, ... 14993, 14994
-
-			var result = Program.GetShiftPositionAlt(sample);
-
-			Assert.Equal(expected: 100_000, actual: result);
+			yield return new object[] { new int[] { 15, 16, 18, 20, 1, 2, 5, 6, 7, 8, 11, 12 }, 4};
+			yield return new object[] { new int[] { 5, 6, 7, 8, 11, 12, 15, 16, 18, 1, 2, 3 },9 };
+			yield return new object[] { new int[] { 5, 6, 7, 8, 11, 12, 15, 16, 18, 20, 1, 2 }, 10 };
+			yield return new object[] { new int[] { 11, 1, 2, 5 }, 1 };
+			yield return new object[] { new int[] { 5, 6, 1, 2 }, 2 };
+			yield return new object[] { new int[] { 7, 11, 1, 2, 5 }, 2 };
+			yield return new object[] { new int[] { 1, 2, 5, 6, 7, 8, 11, 12, 15, 16, 18, 20 }, 0 };
+			yield return new object[] { new int[] { 5 }, 0 };
+			var part1 = Enumerable.Range(15_000, 100_000);
+			var part2 = Enumerable.Range(0, 14_995);
+			var arr = part1.Concat(part2).ToArray();
+			yield return new object[] { arr, 100_000 };
 		}
 	}
 }
