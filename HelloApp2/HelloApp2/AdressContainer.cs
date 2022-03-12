19ab75b7369fbdeb64a524c8d5974c87f519bd75
Art <bartalamu@list.ru>
2022-03-12 20:01:31 +0500

    v0.1 промежуточный результат

---
+++ HelloApp2/HelloApp2/AdressContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloApp2
+{
+    public class AdressContainer
+    {
+            public const string Configuration = "Configuration";
+            public string Adress { get; set; } = String.Empty;
+    }
+}
---
+++ HelloApp2/HelloApp2/Program.cs
@@ -0,0 +1,28 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+builder.Services.Configure<HelloApp2.AdressContainer>(
+    builder.Configuration.GetSection(HelloApp2.AdressContainer.Configuration));
+
+
+builder.Services.AddTransient<HelloApp2.AdressContainer>();
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
