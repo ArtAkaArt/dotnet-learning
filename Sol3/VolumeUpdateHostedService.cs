24b34277d492027f611464819618fa8bfcdd3060
Art <bartalamu@list.ru>
2022-04-27 21:44:58 +0500

    v2.0

--- Sol3/VolumeUpdateHostedService.cs
+++ Sol3/VolumeUpdateHostedService.cs
@@ -3,61 +3,60 @@ using Sol3.Profiles;
 
 namespace Sol3
 {
-    public class VolumeUpdateHostedService : IHostedService, IDisposable
+    public class VolumeUpdateHostedService : BackgroundService
     {
         private readonly ILogger<VolumeUpdateHostedService> logger;
         private readonly IServiceScopeFactory scopeFactory;
-        private Timer timer = null!;
         private Random rnd;
         private readonly IMapper mapper;
+        private readonly IHostApplicationLifetime lifetime;
 
-        public VolumeUpdateHostedService(ILogger<VolumeUpdateHostedService> logger, IServiceScopeFactory scopeFactory, IMapper mapper)
+        public VolumeUpdateHostedService(ILogger<VolumeUpdateHostedService> logger, IServiceScopeFactory scopeFactory, IMapper mapper, IHostApplicationLifetime lifetime)
         {
             this.logger = logger;
             this.scopeFactory = scopeFactory;
             rnd = new();
             this.mapper = mapper;
+            this.lifetime = lifetime;
         }
-        public Task StartAsync(CancellationToken cancellationToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            logger.LogInformation("Timed Hosted Service running.");
-
-            timer = new Timer(VolumesUpdateAsync, null, TimeSpan.Zero,
-                TimeSpan.FromSeconds(30));
-
-            return Task.CompletedTask;
-        }
-        private async void VolumesUpdateAsync(object? state)
-        {
-            using (var scope = scopeFactory.CreateScope())
+            if (!await WaitForAppStartup(lifetime, stoppingToken))
+                return;
+            while (!stoppingToken.IsCancellationRequested)
             {
-                var repo = scope.ServiceProvider.GetRequiredService<FacilityRepo>();
-                var tanks = await repo.GetAllTanks();
-                //logger.LogInformation("Начало foreach---------------------------------------");
-                foreach (var tank in tanks)
+                using (var scope = scopeFactory.CreateScope())
                 {
-                    var tankUpd = mapper.Map<TankDTO>(tank);
-                    var modification = rnd.Next(-10, 10);
-                    //logger.LogInformation($"Volume до изменения = {tankUpd.Volume} 10% = {modification}");
-                    tankUpd.Volume += (int) (tankUpd.Volume /100.0 * modification);
-                    //logger.LogInformation($"Измененный Volume = {tankUpd.Volume} 10% = {modification}");
-                    if (tankUpd.Volume > tankUpd.Maxvolume || tankUpd.Volume < 0)
-                        logger.LogError("Првевышение предела Volume");
-                    else await repo.UpdateTank(tank, tankUpd);
+                    var repo = scope.ServiceProvider.GetRequiredService<FacilityRepo>();
+                    var tanks = await repo.GetAllTanks();
+                    //logger.LogInformation("Начало foreach---------------------------------------");
+                    foreach (var tank in tanks)
+                    {
+                        var tankUpd = mapper.Map<TankDTO>(tank);
+                        var modification = rnd.Next(-10, 10);
+                        //logger.LogInformation($"Volume до изменения = {tankUpd.Volume} 10% = {modification}");
+                        tankUpd.Volume += (int)(tankUpd.Volume / 100.0 * modification);
+                        //logger.LogInformation($"Измененный Volume = {tankUpd.Volume} 10% = {modification}");
+                        if (tankUpd.Volume > tankUpd.Maxvolume || tankUpd.Volume < 0)
+                            logger.LogError($"Првевышение предела Volume {tankUpd.Volume} больше чем MaxVolume {tankUpd.Maxvolume} или меньше нуля");
+                        else await repo.UpdateTank(tank, tankUpd);
+                    }
                 }
+                await Task.Delay(600_000, stoppingToken);
             }
         }
-
-        public Task StopAsync(CancellationToken cancellationToken)
+        //честно стырил с хабра, но разобрался зачем оно нужно
+        static async Task<bool> WaitForAppStartup(IHostApplicationLifetime lifetime, CancellationToken stoppingToken)
         {
-            logger.LogInformation("Timed Hosted Service is stopping.");
-            timer?.Change(Timeout.Infinite, 0);
-            return Task.CompletedTask;
-        }
+            var startedSource = new TaskCompletionSource();
+            using var reg1 = lifetime.ApplicationStarted.Register(() => startedSource.SetResult());
 
-        public void Dispose()
-        {
-            timer?.Dispose();
-        }
+            var cancelledSource = new TaskCompletionSource();
+            using var reg2 = stoppingToken.Register(() => cancelledSource.SetResult());
+
+            var completedTask = await Task.WhenAny(startedSource.Task, cancelledSource.Task).ConfigureAwait(false);
+
+            return completedTask == startedSource.Task;
+        } 
     }
 }
