a0e266377d1afe0f71b01b3eeeb00dc2b8f62fd7
Art <bartalamu@list.ru>
2022-05-19 06:04:57 +0500

    v0.1 считывание постов с сайта + тестовый вывод в консоль

---
+++ Sol5_1_Collections/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sol5_1_Collections
+{
+    public class Post
+    {
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = null!;
+        [JsonPropertyName("body")]
+        public string Body { get; set; } = null!;
+    }
+}
---
+++ Sol5_1_Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+
+namespace Sol5_1_Collections;
+public class Program {
+
+    static readonly HttpClient client = new HttpClient();
+    static async Task Main()
+    {
+        List<Post> list = new();
+        try
+        {
+            var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+            response.EnsureSuccessStatusCode();
+            var responseText = await response.Content.ReadAsStringAsync();
+            list = JsonSerializer.Deserialize<List<Post>>(responseText);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        list.ForEach(post => Console.WriteLine(post.Title));
+        Console.ReadKey();
+    }
+}
