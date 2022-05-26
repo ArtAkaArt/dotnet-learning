8900667a6fcb094ed80500275ddd53d5b12f7724
Art <bartalamu@list.ru>
2022-05-26 16:27:31 +0500

    v3.1 все работает, но семафор не там

--- Sol5_1_Collections/Program.cs
+++ Sol5_1_Collections/Program.cs
@@ -2,38 +2,83 @@
 
 namespace Sol5_1_Collections;
 public class Program {
-    static List<Post> list = new();
+    static volatile List<Post> list = new();
+    private static SemaphoreSlim semaphore;
+    static HttpClient client = new HttpClient();
     static async Task Main()
     {
-        List <Task> tasklist = new();
+        //получаю лист в одном треде, для сравнения с ним
+        List<Post> listInThread = new();
+        try
+        {
+            var response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+            response.EnsureSuccessStatusCode();
+            var responseText = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            listInThread = JsonSerializer.Deserialize<List<Post>>(responseText, options);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
 
-        //Task task = new Task(async() => await GetPostAsync(1, list));
-        Task task = GetPostAsync(1, list);
-        //task.Start();
-        task.Wait();
-        Console.WriteLine(list.Count()+"----------");
+        semaphore = new SemaphoreSlim(5);
+        List<Func<Task>> funcs = new();
+        
+        for (int i = 1; i <= 100; i++)
+        {
+            var count = i; // !!!! крайневажная переменная без нее все работает неправильно
+            Console.WriteLine(i);
+            funcs.Add(new( async () => await GetPostAsync(count, list)));
+        }
+        funcs.RunInParallel(5);
+        Console.WriteLine("Main thread - после RunInParallel");
+
+        //дааа... очень топорная сверка
+        for (int i = 1; i <= 100; i++)
+        {
+            var post1 = listInThread.FirstOrDefault(x => x.Id == i);
+            var post2 = list.FirstOrDefault(x => x.Id == i);
+            var isEqual = post1.Id == post2.Id && post1.Body == post2.Body && post1.Title == post2.Title && post1.UserId == post2.UserId;
+            Console.WriteLine(isEqual);
+        }
         Console.ReadKey();
-
     }
     static async Task GetPostAsync(int number, List<Post> list)
     {
+        semaphore.Wait();
+        Console.WriteLine($"Task started _ {semaphore.CurrentCount}");
         
-        HttpClient client = new HttpClient();
-        var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{number}"); // здесь происходит переход на завергение программы
+        var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{number}");
         var responseText = await response.Content.ReadAsStringAsync();
-
         var options = new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
         };
-        Post post = JsonSerializer.Deserialize<Post>(responseText, options);
-        Console.WriteLine(post.Body);
+        var post = JsonSerializer.Deserialize<Post>(responseText, options);
+        Console.WriteLine("Task ended_" +post.Id);
         list.Add(post);
+        semaphore.Release();
     }
 }
 public static class MyTaskListExtention
 {
-    public static async Task RunInParallel(this IEnumerable<Task> taskList, int vol = 4)
+    /* передал значение по умолчанию, но чтобы передать его в семафор, нужно еще сам семафор объявлять в методе и => сами функции тут же
+     * это выглядит немного странно в плане функциональности самого метода
+     */
+    public static void RunInParallel(this IEnumerable<Func<Task>> functs, int vol = 4)
     {
+        List<Task> tasks = new();
+        foreach (var func in functs)
+        {
+            tasks.Add(func.Invoke());
+            Console.WriteLine("Task added");
+        }
+        Console.WriteLine(tasks.Count()+" Task count");
+        Task.WaitAll(tasks.ToArray());
+        Console.WriteLine("End in RunInParallel");
     }
 }
