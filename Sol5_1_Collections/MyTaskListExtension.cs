ca03ef28485a1b6c3587d479ff4ec2f8e1f2889a
Art <bartalamu@list.ru>
2022-06-18 21:58:14 +0500

    v4.4 Доработка по комментариям

---
+++ Sol5_1_Collections/MyTaskListExtension.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+
+namespace Sol5_1_Collections
+{
+    public static class MyTaskListExtention
+    {
+        /// <summary>
+        /// Асинхронный метод возвращающий коллекцию объектов, хранящихся в IReadOnlyCollection. Может принимать параметры int maxTasks и bool throwException, имеющие
+        /// значения по умолчанию 4 и false.</summary>
+        /// <typeparam name="T"></typeparam> Тип возвращаемого объекта.
+        /// <param name="maxTasks"> Определяет количество одновременно исполняемых потоков для получения объектов</param>
+        /// <param name="throwException">Определяет будет ли выброшена первая ошибка при получении объектов и остановлен метод.
+        /// В режиме false будут переданы все объекты, которые удалось получить и все полученные ошибки через AggregateException</param>
+        /// <returns></returns>
+        /// <exception cref="AggregateException"></exception>
+        public static async Task<IReadOnlyCollection<T>> RunInParallel<T>(this IEnumerable<Func<Task<T>>> functs, int maxTasks = 4, bool throwException = false)
+        {
+            ConcurrentBag<T> list = new();
+            var semaphore = new SemaphoreSlim(maxTasks);
+            var tasks = new List<Task>();
+            foreach (var func in functs)
+            {
+                var task = await Task.Factory.StartNew(async () => {
+                    await semaphore.WaitAsync();
+                    try
+                    {
+                        list.Add(await func.Invoke());
+                    }
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                });
+                tasks.Add(task);
+            }
+            Console.WriteLine(tasks.Count(t => t != null) + " Task count");
+            try
+            {
+                await Task.WhenAll(tasks);
+            }
+            catch (Exception ex)
+            {
+                if (throwException)
+                {
+                    var exceptions = tasks.Where(t => t.Exception != null)
+                                          .Select(t => t.Exception);
+                    throw new AggregateException(exceptions);
+                }
+            }
+            Console.WriteLine("End in RunInParallel");
+            return list;
+        }
+        public static async IAsyncEnumerable<Post> RunInParallelAlt(this IEnumerable<Func<Task<Post>>> functs, int maxTasks = 4, bool throwException = false)
+        {
+            List<Exception> exList = new();
+            var semaphore = new SemaphoreSlim(maxTasks);
+            foreach (var func in functs)
+            {
+                var post = new Post();
+                var hadResult = true;
+                try
+                {
+                    post = await Task.Run(async () =>
+                    {
+                        await semaphore.WaitAsync();
+                        try
+                        {
+                            return await func.Invoke();
+                        }
+                        finally
+                        {
+                            semaphore.Release();
+                        }
+                    });
+
+                }
+                catch (Exception ex)
+                {
+                    if (throwException) throw new AggregateException(ex);
+                    exList.Add(ex);
+                    hadResult = false;
+                }
+                if (hadResult) yield return post;
+            };
+            if (exList.Count > 0) throw new AggregateException(exList);// возвращения списка ошибок, если они возникли
+        }
+    }
+}
--- Sol5_1_Collections/Program.cs
+++ Sol5_1_Collections/Program.cs
@@ -34,7 +34,7 @@ public class Program {
             funcs.Add(new( async () => await GetPostAsync(count)));
         }
         var isListsEqual = true;
-        /*
+        
         try
         {
             var list = await funcs.RunInParallel(5, true);
@@ -62,7 +62,7 @@ public class Program {
         {
             Console.WriteLine("Ex "+ex.Message);
         }
-        */
+        /*
         var asyncEnumList = funcs.RunInParallelAlt(5);
         var postsList = new List<Post>();
         try
@@ -84,13 +84,14 @@ public class Program {
             }
             Console.WriteLine(postsList.Count); // получение и проверка списка постов из IAsyncEnum
         }
+        */
         Console.ReadKey();
     }
     static async Task<Post> GetPostAsync(int number)
     {
         Console.WriteLine($"Task started");
         // генерация нескольких ошибок
-        if (number % 10 == 0)  throw new Exception("Ошибка при получении поста номер: "+number);
+        //if (number % 10 == 0)  throw new Exception("Ошибка при получении поста номер: "+number);
         var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{number}");
         response.EnsureSuccessStatusCode();
         var responseText = await response.Content.ReadAsStringAsync();
@@ -99,79 +100,8 @@ public class Program {
             PropertyNameCaseInsensitive = true
         };
         var post = JsonSerializer.Deserialize<Post>(responseText, options);
+        await Task.Delay(1500);
         Console.WriteLine("Task ended_" +post.Id);
         return post;
-    }
-}
-public static class MyTaskListExtention
-{
-    public static async Task<IReadOnlyCollection<Post>> RunInParallel(this IEnumerable<Func<Task<Post>>> functs, int vol = 4, bool throwException = false )
-    {
-        ConcurrentBag<Post> list = new ();
-        var semaphore = new SemaphoreSlim(vol);
-        var tasks = new List<Task>();
-        foreach (var func in functs)
-        {
-            var task = await Task.Factory.StartNew(async () => {
-                await semaphore.WaitAsync();
-                try
-                {
-                    list.Add(await func.Invoke());
-                }
-                finally
-                {
-                    semaphore.Release();
-                }
-            });
-            tasks.Add(task);
-        }
-        Console.WriteLine(tasks.Count(t => t != null) +" Task count");
-        try
-        {
-            await Task.WhenAll(tasks);
-        }
-        catch (Exception ex)
-        {
-            if (throwException)
-            {
-                var exceptions = tasks.Where(t => t.Exception != null)
-                                      .Select(t => t.Exception);
-                throw new AggregateException(exceptions);
-            }
-        }
-        Console.WriteLine("End in RunInParallel");
-        return list;
-    }
-    public static async IAsyncEnumerable<Post> RunInParallelAlt(this IEnumerable<Func<Task<Post>>> functs, int vol = 4, bool throwException = false)
-    {
-        List<Exception> exList =new();
-        var semaphore = new SemaphoreSlim(vol);
-        foreach (var func in functs)
-        {
-            var post = new Post();
-            var hadResult = true;
-            try
-            {
-                post = await Task.Run(async () =>
-                {
-                    await semaphore.WaitAsync();
-                    try
-                    {
-                        return await func.Invoke();
-                    }
-                    finally
-                    {
-                        semaphore.Release();
-                    }
-                });
-            }catch (Exception ex)
-            {
-                if(throwException) throw new AggregateException (ex);
-                exList.Add(ex);
-                hadResult = false;
-            }
-            if (hadResult) yield return post;
-        };
-        if (exList.Count>0)throw new AggregateException(exList);// возвращения списка ошибок, если они возникли
     }
 }
