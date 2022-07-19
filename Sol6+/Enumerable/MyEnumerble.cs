25f9274fd9c20bb02c005330f95b92803aab86f3
Art <bartalamu@list.ru>
2022-07-19 16:36:35 +0500

    v1 Enumerable & Iterator

--- Sol6+/Enumerable/MyEnumerble.cs
+++ Sol6+/Enumerable/MyEnumerble.cs
@@ -4,15 +4,32 @@ namespace Enumerable
 {
     public class MyEnumerable : IEnumerable<int>
     {
+<<<<<<< HEAD
         public IEnumerator<int> GetEnumerator() => new MyEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+=======
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new MyEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyEnumerator();
+        }
+>>>>>>> 5c50d95 (v1 Enumerable & Iterator)
     }
     public class MyEnumerator : IEnumerator<int>
     {
         private int index = -1;
         private bool doReverse = false;
+<<<<<<< HEAD
         public int Current => index;
+=======
+        public int Current { get { return index; } }
+
+>>>>>>> 5c50d95 (v1 Enumerable & Iterator)
         object IEnumerator.Current => Current;
 
         public void Dispose()
