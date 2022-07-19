dd26be4864bdc5b45663bf74528170a6a9d9ee5e
Art <bartalamu@list.ru>
2022-07-19 16:36:35 +0500

    v1 Enumerable & Iterator

---
+++ Sol6+/Enumerable/MyEnumerble.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace Enumerable
+{
+    public class MyEnumerable : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new MyEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyEnumerator();
+        }
+    }
+    public class MyEnumerator : IEnumerator<int>
+    {
+        private int index = -1;
+        private bool doReverse = false;
+        public int Current { get { return index; } }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (doReverse)
+            {
+                if (index == 0) return false;
+                index--;
+                return true;
+            }
+            index++;
+            if (index == 100) doReverse = true;
+            return true;
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
---
+++ Sol6+/Enumerable/MyIterator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerable
+{
+    public class MyIterator
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                yield return i;
+            }
+            for (int i = 99; i >= 0; i--)
+            {
+                yield return i;
+            }
+        }
+    }
+}
