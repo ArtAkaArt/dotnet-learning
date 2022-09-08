6840095ce91cc572550dbd637c05a9b842bf38df
Art <bartalamu@list.ru>
2022-09-08 17:23:52 +0500

    v0.3 IFacility

--- FacilityRepoEF/Factory.cs
+++ FacilityRepoEF/Factory.cs
@@ -1,6 +1,6 @@
 ﻿namespace FacilityContextLib
 {
-    public class Factory
+    public class Factory : IFacility
     {
         public int Id { get; set; }
         public string? Name { get; set; }
